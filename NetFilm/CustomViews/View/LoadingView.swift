b6ae1bb69cf025a8f5e93f5d26afc85e16e73a82
Muhammed Faruk <faruk.sogut@btpro.net>
2023-08-28 22:55:44 +0300

    ** Add ** *.gitignore

---
+++ NetFilm/CustomViews/View/LoadingView.swift
@@ -0,0 +1,8 @@
+//
+//  LoadingView.swift
+//  NetFilm
+//
+//  Created by Muhammed Faruk Söğüt on 28.08.2023.
+//
+
+import Foundation
---
+++ NetFilm/Utilities/Protocols/LoadingShowable.swift
@@ -0,0 +1,8 @@
+//
+//  LoadingShowable.swift
+//  NetFilm
+//
+//  Created by Muhammed Faruk Söğüt on 28.08.2023.
+//
+
+import Foundation
