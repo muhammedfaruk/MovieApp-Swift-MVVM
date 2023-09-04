74dbcfc6ae112f454a48e117d91234e2f13230f8
Muhammed Faruk <faruk.sogut@btpro.net>
2023-09-04 22:46:17 +0300

    ** Add ** *Api Key

---
+++ NetFilm/Service/Base/ApiKey.swift
@@ -0,0 +1,12 @@
+//
+//  ApiKey.swift
+//  NetFilm
+//
+//  Created by Muhammed Faruk Söğüt on 2.09.2023.
+//
+
+import Foundation
+
+class ApiKey {
+    static var apiKey = "7dd354fbedc4932b4863756f9df41e26"
+}
